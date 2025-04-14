df0fa4fd7d510884bc07ab1a2d75b094efe4f95a
jackpencwby <kajonyod.jack@gmail.com>
2025-04-14 15:53:40 +0700

    upload image to cloudinary finish

---
+++ src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/controller/UploadImageController.java
@@ -0,0 +1,28 @@
+package com.jacknextshop.jacknextshop_ecommerce_backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.jacknextshop.jacknextshop_ecommerce_backend.service.CloudinaryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class UploadImageController {
+
+    @Autowired
+    private CloudinaryService cloudinaryService;
+
+    @PostMapping("/api/upload")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile image) {
+        try {
+            String imageUrl = cloudinaryService.uploadImage(image);
+            return ResponseEntity.ok(imageUrl);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}

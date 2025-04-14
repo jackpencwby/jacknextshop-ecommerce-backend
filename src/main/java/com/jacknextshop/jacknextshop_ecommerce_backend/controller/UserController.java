924ca56239438587ded595658c6d364a2ff8e1ba
SNeXT <snext2004@gmail.com>
2025-04-14 21:28:41 +0700

    remove unused import

--- src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/controller/UserController.java
+++ src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/controller/UserController.java
@@ -7,11 +7,8 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.jacknextshop.jacknextshop_ecommerce_backend.dto.APIResponseDTO;
 import com.jacknextshop.jacknextshop_ecommerce_backend.dto.user.UserRequestBodyDTO;

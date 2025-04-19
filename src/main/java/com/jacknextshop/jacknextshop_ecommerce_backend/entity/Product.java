9cb9d8865d578944c9487bee89ae228daea4f6ef
SNeXT <snext2004@gmail.com>
2025-04-19 16:07:06 +0700

    delete unused import

--- src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/entity/Product.java
+++ src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/entity/Product.java
@@ -4,8 +4,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;

6f601e6b92c8a64cedc34b7128979d9ec66e6464
jackpencwby <kajonyod.jack@gmail.com>
2025-06-08 23:07:56 +0700

    fix path of  HelloController for test

--- src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/controller/HelloController.java
+++ src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/controller/HelloController.java
@@ -2,10 +2,12 @@ package com.jacknextshop.jacknextshop_ecommerce_backend.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }

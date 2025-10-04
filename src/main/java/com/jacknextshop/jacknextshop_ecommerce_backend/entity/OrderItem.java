15fe2bc9bcc80db45c1e49dc1a6ae1213ffebc6a
jackpencwby <kajonyod.jack@gmail.com>
2025-10-04 13:58:11 +0700

    Add OrderItem entity with fields for order item management

---
+++ src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/entity/OrderItem.java
@@ -0,0 +1,34 @@
+package com.jacknextshop.jacknextshop_ecommerce_backend.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_item_id")
+    private UUID orderItemId;
+
+    @Column(name = "amount", nullable = false)
+    private int amount;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}

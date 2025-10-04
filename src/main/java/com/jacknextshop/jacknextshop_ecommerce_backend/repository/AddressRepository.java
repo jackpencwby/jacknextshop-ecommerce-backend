b25368eeefa6cd847307015538e72f57d0713d7f
jackpencwby <kajonyod.jack@gmail.com>
2025-10-04 16:29:28 +0700

    Add AddressRepository interface for managing Address entities

---
+++ src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/repository/AddressRepository.java
@@ -0,0 +1,12 @@
+package com.jacknextshop.jacknextshop_ecommerce_backend.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jacknextshop.jacknextshop_ecommerce_backend.entity.Address;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}

8bd5db4facd3ba4556075fd19b679fe4cab346a5
jackpencwby <kajonyod.jack@gmail.com>
2025-10-04 12:25:23 +0700

    first commit

---
+++ src/main/java/com/jacknextshop/jacknextshop_ecommerce_backend/HelloController.java
@@ -0,0 +1,14 @@
+package com.jacknextshop.jacknextshop_ecommerce_backend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/jacknextshop/jacknextshop_ecommerce_backend/JacknextshopEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacknextshop.jacknextshop_ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacknextshopEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
